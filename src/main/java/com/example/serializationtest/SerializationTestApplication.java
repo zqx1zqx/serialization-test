0d034c3fedbea8de459d6e7678d0ee3b8516492d
张清熙 <jojo.zhang@pkfare.com>
2023-10-08 14:34:56 +0800

    feat:first commit

---
+++ src/main/java/com/example/serializationtest/SerializationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.serializationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerializationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerializationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serializationtest/SerializationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serializationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerializationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
